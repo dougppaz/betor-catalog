07804d765981e8f205dbf9384526f0b92d98d595
Douglas Paz <eu@douglaspaz.com>
2025-09-18 20:41:37 -0300

    feat: add item title and provider name

--- .eleventy.js
+++ .eleventy.js
@@ -3,10 +3,19 @@ const { simpleHash } = require('./src/utils')
 module.exports = eleventyConfig => {
   eleventyConfig.addPassthroughCopy('src/static')
 
-  eleventyConfig.addFilter("itemUrl", item => {
+  eleventyConfig.addFilter('itemUrl', item => {
     const idsValue = `${item.imdb_id}-${item.tmdb_id}`
     const idsHash = simpleHash(idsValue)
     return `/${item.item_type}/${idsHash}/`
+  })
+
+  eleventyConfig.addFilter('itemTitle', item => (item.info?.title || item.info?.name))
+
+  eleventyConfig.addFilter('providerName', slug => {
+    if (slug === "comando-torrents") return "Comando"
+    if (slug === "bludv") return "BLUDV"
+    if (slug === "torrent-dos-filmes") return "Torrent dos Filmes"
+    return slug
   })
 
   return {
