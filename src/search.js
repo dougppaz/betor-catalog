8adea8b1afa92fbdcbd555cdef8c24adfdf07265
Douglas Paz <eu@douglaspaz.com>
2025-10-23 11:53:29 -0300

    fix: enable tv search

--- src/search.js
+++ src/search.js
@@ -6,19 +6,22 @@ import renderBase from './components/pages/base.js'
 const fuse = new Fuse(items, {
   keys: [
     'info.title',
-    'info.original_title'
+    'info.original_title',
+    'info.name',
+    'info.original_name'
   ],
   threshold: 0.2
 })
 
 const fetch = async (request, env, ctx) => {
   const url = new URL(request.url)
-  const q = url.searchParams.get('q')
+  const q = url.searchParams.get('q') || ''
   if (!q.trim()) {
     return new Response(null, { status: 404 })
   }
   const results = fuse.search(q)
   const itemsHTML = renderItems(results)
+  console.log(results)
   return new Response(
     renderBase(itemsHTML, { title: `Resultados da busca "${q}" - Catálogo Betor` }),
     { headers: { 'Content-Type': 'text/html; charset=utf-8' } }
